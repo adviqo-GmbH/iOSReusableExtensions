7e5697a400a42ed77a3149237274d1f568b309ed
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2019-01-14 17:45:54 +0100

    Initial Commit

---
+++ iOSReusableExtensions/iOSReusableExtensions.h
@@ -0,0 +1,19 @@
+//
+//  iOSReusableExtensions.h
+//  iOSReusableExtensions
+//
+//  Created by Oleksandr Pronin on 14.01.19.
+//  Copyright © 2019 adviqo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSReusableExtensions.
+FOUNDATION_EXPORT double iOSReusableExtensionsVersionNumber;
+
+//! Project version string for iOSReusableExtensions.
+FOUNDATION_EXPORT const unsigned char iOSReusableExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSReusableExtensions/PublicHeader.h>
+
+
