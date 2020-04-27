3d1e7a9c0890842f6604e59033df0b920bf7be30
Aleksei Neronov <aleksei.neronov@adviqo.com>
2020-04-27 14:05:01 +0200

    1.0.12 Minor changes

--- iOSReusableExtensions/Extensions/String/String+Extension.swift
+++ iOSReusableExtensions/Extensions/String/String+Extension.swift
@@ -87,14 +87,3 @@ public extension String
         }
     }
 }
-// Check later
-//@objc public extension NSString {
-//    func stringWith(format: String) -> NSString? {
-//        guard let mask = try? Mask(format: format) else {
-//            return nil
-//        }
-//        let valueString = self as String
-//        let formattedString = mask.apply(toText: CaretString(string: valueString, caretPosition: valueString.endIndex)).formattedText.string
-//        return formattedString as NSString
-//    }
-//}
